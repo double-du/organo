625ad482e079bbd732c13437af4f1fe732d6d482
Eduardo Furlaneti <eduardohppa@gmail.com>
2023-06-07 17:49:41 -0300

    fix: Melhorando condicional

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -3,7 +3,7 @@ import './Time.css';
 
 const Time = (props) => {
     return (
-        (props.colaboradores.length > 0 ) && <section className={`time ${props.classe}`}>
+        (props.colaboradores.length > 0 ) ? <section className={`time ${props.classe}`}>
             <h3>{props.nome}</h3>
             <div className='cards'>
                 {props.colaboradores.map(
@@ -17,6 +17,7 @@ const Time = (props) => {
                 )}
             </div>
         </section>
+        : ''
     );
 }
 

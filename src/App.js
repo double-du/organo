5664cd36547f141821e628a70df85456317cd97e
Eduardo Furlaneti <eduardohppa@gmail.com>
2023-06-12 17:37:14 -0300

    Adicionando rodape

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
 import Time from './components/Time';
+import Rodape from './components/Rodape/Rodape';
 
 function App() {
 
@@ -57,6 +58,7 @@ function App() {
             /> 
         )
       }
+    <Rodape />
     </div>
   );
 }
